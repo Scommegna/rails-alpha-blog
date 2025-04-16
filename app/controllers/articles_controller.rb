b3c8b982350ce5b5eb943830cc8aef6f3bd06fc8
Lucas Scommegna <scommegnal@hotmail.com>
2025-04-16 14:39:22 -0300

    feat: post method to create article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,5 +11,10 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+
+    @article.save
+
+    redirect_to @article
   end
 end
